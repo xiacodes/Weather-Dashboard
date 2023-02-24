d215e006590cfdc51767ffec4003c4a34fe5833c
Isabelle Zennah <izennah@griffithsandarmour.com>
2023-02-24 12:40:54 +0000

    Working on the second option function

--- starter/script.js
+++ starter/script.js
@@ -12,12 +12,10 @@ searchBtn.on("click", function (event) {
 	event.preventDefault();
 	var city = searchInput.val();
 	weatherLoad(city);
-	alert(city);
 });
 
 function weatherLoad(city) {
 	var query = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey + "&units=metric";
-	alert(query);
 
 	if (city != "") {
 		$.ajax({
@@ -30,25 +28,33 @@ function weatherLoad(city) {
 			cityInfo.lon = response.lon;
 
 			//Load up the individual city
-			todaysWeatherReport(response);
+			weatherReport(response, 1);
 			fiveDayWeatherReport(response);
 		});
 	}
 }
 
-function todaysWeatherReport(response) {
-	const fullDate = moment.unix(response.dt).format("DD/MM/YYYY");
-	const image = response.weather[0].icon;
+function weatherReport(response, option) {
+	if (option === 1) {
+		const fullDate = moment.unix(response.dt).format("DD/MM/YYYY");
+		const image = response.weather[0].icon;
 
-	// Create elements for current weather conditions
-	const cityTitleElement = $("<h3>").text(response.name);
-	const dateElement = $('<span class="todays-date">').text(" (" + fullDate + ")");
-	const iconElement = $("<img>").attr("src", "https://openweathermap.org/img/wn/" + image + ".png");
-	const tempElement = $("<p>").text(`Temp: ${response.main.temp} °C`);
-	const windElement = $("<p>").text(`Wind: ${response.wind.speed} KPH`);
-	const humidElement = $("<p>").text(`Humidity: ${response.main.humidity}%`);
+		// Creating the elements to add to the #today div
+		const cityTitleElement = $("<h3>").text(response.name);
+		const dateElement = $('<span class="todays-date">').text(" (" + fullDate + ")");
+		const iconElement = $("<img>").attr("src", "https://openweathermap.org/img/wn/" + image + ".png");
+		const tempElement = $("<p>").text(`Temp: ${response.main.temp} °C`);
+		const windElement = $("<p>").text(`Wind: ${response.wind.speed} KPH`);
+		const humidElement = $("<p>").text(`Humidity: ${response.main.humidity}%`);
 
-	// Appending time!!
-	cityTitleElement.append(dateElement, iconElement);
-	todayForecast.empty().append(cityTitleElement, tempElement, windElement, humidElement);
+		// Appending time!!
+		cityTitleElement.append(dateElement, iconElement);
+		todayForecast.empty().append(cityTitleElement, tempElement, windElement, humidElement);
+	}
+	if (option === 2) {
+		// Location elements
+		var latitude = response.coord.lat;
+		var longitude = response.coord.lon;
+		var query = "https://api.openweathermap.org/data/2.5/forecast?lat=" + latitude + "&lon=" + longitude + "&appid=" + apiKey + "&units=metric";
+	}
 }
