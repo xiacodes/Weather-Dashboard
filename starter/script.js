8fb71b68d27e1da763ff04780c441f6303ecb74a
Isabelle Zennah <izennah@griffithsandarmour.com>
2023-02-24 08:30:37 +0000

    Getting the search btn to work

--- starter/script.js
+++ starter/script.js
@@ -1,7 +1,25 @@
 // Variables
-
+var searchInput = $("#search-input");
+var searchBtn = $(`#search-button`);
 var fiveDayForecast = $(`#forecast`);
 var todayForecast = $(`#today`);
 
 // API Information
 var apiKey = "6a5e21ce19fc0952eac333669721b7de";
+
+// ------------------------------------------- //
+searchBtn.on("click", function (event) {
+	event.preventDefault();
+	var city = searchInput.val();
+	// weatherLoad(city);
+	alert(city);
+});
+
+// weatherLoad(city) {
+//     var query = "https://api.openweathermap.org/data/2.5/weather?q=" + city +
+//     "&appid=" +  apiKey + "&units=metric";
+
+//     // if (city == "") {
+
+//     // }
+// }
