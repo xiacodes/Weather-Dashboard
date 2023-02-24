514c155730313a495dde161a3d505ad150f86eeb
Isabelle Zennah <izennah@griffithsandarmour.com>
2023-02-24 11:18:48 +0000

    Got the current weather to work!!!

--- starter/script.js
+++ starter/script.js
@@ -20,6 +20,35 @@ function weatherLoad(city) {
 	alert(query);
 
 	if (city != "") {
-		alert(`Hello!`);
+		$.ajax({
+			url: query,
+			method: "GET",
+		}).then(function (response) {
+			var cityInfo = {};
+			cityInfo.name = response.name;
+			cityInfo.lat = response.lat;
+			cityInfo.lon = response.lon;
+
+			//Load up the individual city
+			todaysWeatherReport(response);
+			fiveDayWeatherReport(response);
+		});
 	}
+}
+
+function todaysWeatherReport(response) {
+	const fullDate = moment.unix(response.dt).format("DD/MM/YYYY");
+	const image = response.weather[0].icon;
+
+	// Create elements for current weather conditions
+	const cityTitleElement = $("<h3>").text(response.name);
+	const dateElement = $('<span class="todays-date">').text(" (" + fullDate + ")");
+	const iconElement = $("<img>").attr("src", "https://openweathermap.org/img/wn/" + image + ".png");
+	const tempElement = $("<p>").text(`Temp: ${response.main.temp} °C`);
+	const windElement = $("<p>").text(`Wind: ${response.wind.speed} KPH`);
+	const humidElement = $("<p>").text(`Humidity: ${response.main.humidity}%`);
+
+	// Appending time!!
+	cityTitleElement.append(dateElement, iconElement);
+	todayForecast.empty().append(cityTitleElement, tempElement, windElement, humidElement);
 }
