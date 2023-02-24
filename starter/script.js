6ea6281fc10247cec0066378485858091a38e915
Isabelle Zennah <izennah@griffithsandarmour.com>
2023-02-24 08:33:30 +0000

    Testing the WeatherLoad()

--- starter/script.js
+++ starter/script.js
@@ -11,15 +11,15 @@ var apiKey = "6a5e21ce19fc0952eac333669721b7de";
 searchBtn.on("click", function (event) {
 	event.preventDefault();
 	var city = searchInput.val();
-	// weatherLoad(city);
+	weatherLoad(city);
 	alert(city);
 });
 
-// weatherLoad(city) {
-//     var query = "https://api.openweathermap.org/data/2.5/weather?q=" + city +
-//     "&appid=" +  apiKey + "&units=metric";
+function weatherLoad(city) {
+	var query = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey + "&units=metric";
+	alert(query);
 
-//     // if (city == "") {
-
-//     // }
-// }
+	if (city != "") {
+		alert(`Hello!`);
+	}
+}
